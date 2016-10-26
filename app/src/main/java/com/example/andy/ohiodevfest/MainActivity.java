2c0b72cdd205352286d6d1cfcc681addaf0420c6
Andy <qazwer8889@gmail.com>
2016-10-26 15:59:43 -0400

    moved some drawables, added icons, and changed the items on the nav drawer

--- app/src/main/java/com/example/andy/ohiodevfest/MainActivity.java
+++ app/src/main/java/com/example/andy/ohiodevfest/MainActivity.java
@@ -2,9 +2,8 @@ package com.example.andy.ohiodevfest;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,8 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.example.andy.ohiodevfest.R;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -69,9 +67,6 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -82,17 +77,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_schedule) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_speakers) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_partners) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_conduct) {
+
+        } else if (id == R.id.nav_settings) {
 
         }
 
